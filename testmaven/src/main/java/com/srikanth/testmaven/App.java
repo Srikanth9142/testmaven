557f53252773aea803e6660f926aadc750dcea24
srikanth9142 <srikanthtumpudi1999@gmail.com>
2020-02-26 15:08:04 +0530

    this is for testing

---
+++ testmaven/src/main/java/com/srikanth/testmaven/App.java
@@ -0,0 +1,16 @@
+package com.srikanth.testmaven;
+import java.util.*;
+import java.io.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println("Welcome "+st);
+    }
+}
